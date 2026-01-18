8d5a588ac72d953a31733030fbd9f3166eea2b6a
zzp ee <2551136251@qq.com>
2026-01-18 21:04:22 +0800

    项目骨架

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -10,17 +10,13 @@ import java.util.*;
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<Map<String, String>> getBotInfo() {
-        List<Map<String, String>> list = new LinkedList<>();
+    public Map<String, String> getBotInfo() {
+
         Map<String, String> bot1 = new HashMap<String, String>();
-        bot1.put("name", "tight");
+        bot1.put("name", "apple");
         bot1.put("rating", "1500");
-        Map<String, String> bot2 = new HashMap<String, String>();
-        bot2.put("name", "apple");
-        bot2.put("rating", "1800");
-        list.add(bot1);
-        list.add(bot2);
-        return list;
+
+        return bot1;
     }
 
 }
